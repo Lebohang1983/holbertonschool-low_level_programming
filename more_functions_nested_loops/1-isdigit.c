3fde4e9d16e827aaacbcba78d388b5b004ce83da
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-02 13:41:57 +0000

    Is

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isdigit - Checks for a digit
+ *@c: char variable
+ *Returns: 1 if c is a digit and 0 otherwise
+ *Decription: Return is not void
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,4 +7,12 @@
  * Decription: return is not void
  */
 int _isupper(int c);
+
+/**
+ * _isdigit - Function that checks for a digit
+ * @c: variable of type char
+ * Return: 1 if c is digit and 0 otherwise
+ * Decription: return is not void
+ */
+int _isdigit(int c);
 #endif
