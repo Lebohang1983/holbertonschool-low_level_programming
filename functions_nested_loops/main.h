4f0bcb8a123262e56dcdc8ae3f557a7809020db5
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-11 23:41:05 +0000

    L

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,10 @@
  *c is the variable
  */
 int _putchar(char c);
+
+/**
+ *prints the alphabet in lowercase followed by a new line
+ *c is the variable that contains the lower case alphabet
+ */
+void print_alphabet(void);
 #endif
