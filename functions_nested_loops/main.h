92a4ea41b6c70707f8987e407668d5691fe500bd
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-12 00:06:48 +0000

    L

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,4 +12,11 @@ int _putchar(char c);
  *c is the variable that contains the lower case alphabet
  */
 void print_alphabet(void);
+
+/**
+ *prints 10 times the alphabet in lowercase followed by a new line
+ *c is the variable that contains the lower case alphabet
+ *i is the interger that shows the number of times the alphabet will be printed
+ */
+void print_alphabet_x10(void);
 #endif
