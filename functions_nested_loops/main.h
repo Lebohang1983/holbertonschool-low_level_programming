ee28864896daeb5ac9367c8739d483a9f4de0ab4
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-13 00:42:06 +0000

    P

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -48,4 +48,13 @@ int print_sign(int n);
  */
 int _abs(int i);
 
+/**
+ * print_last_digit - will print the last digit of a given number.
+ * @n: Number used to find the last digit.
+ * Description: will print the last digit of a given number. If the
+ * number is a negative the value will be return unsigned int.
+ * Return: last digit as int value.
+ */
+int print_last_digit(int n);
+
 #endif
