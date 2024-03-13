07039deec0719737175c9d1f5c92a9f3ee151bbe
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-13 05:34:50 +0000

    j

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -47,5 +47,10 @@ void print_most_numbers(void);
  */
 void more_numbers(void);
 
+/**
+ * print_line - prints a line to a given lenght.
+ * @n: length of the line.
+ */
+void print_line(int n);
 
 #endif
