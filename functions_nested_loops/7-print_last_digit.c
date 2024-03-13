f57583de0af736af8fe7ab07a18f5753f9bfa3b1
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-13 00:50:06 +0000

    P

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,7 @@
 /**
  * print_last_digit - will print the last digit of a given number.
  * @n: Number used to find the last digit.
- * Description: will print the last digit of a given number. If the
- * number is a negative the value will be return unsigned int.
+ * Description: will print the last digit of a given number.
  * Return: last digit as int value.
  */
 int print_last_digit(int n)
