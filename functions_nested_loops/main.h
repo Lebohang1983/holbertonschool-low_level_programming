3014051215999164308563105449ab3e4b4687dd
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-13 01:15:46 +0000

    P

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -68,4 +68,12 @@ void jack_bauer(void);
  */
 void times_table(void);
 
+/**
+ * add - adds two integers together
+ * @a: value to be used for sum.
+ * @b: second value to be use for sum.
+ * Return: int (Sum)
+ */
+int add(int a, int b);
+
 #endif
