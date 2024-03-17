a3e23a210d8cfd72ed2ca6b4271b303382bc0714
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-17 03:51:03 +0000

    K

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@ void print_rev(char *s)
 {
 	int i, n;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *s[i] != '\0'; i++)
 	{
 		continue;
 	}
 	n = i - 1;
 	for (i = n; i >= 0 ; i--)
 	{
-		print_rev(s[i]);
+		print_rev(*s[i]);
 	}
 print_rev('\n');
 return;
