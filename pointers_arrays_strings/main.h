df04d17b4735819a48c379071fd8619db3dd0988
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-17 03:03:45 +0000

    K

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,4 +20,10 @@ void swap_int(int *a, int *b);
   */
 int _strlen(char *s);
 
+/**
+ * _puts - minic the stdio version of puts
+ * @str: the value to process to the stdout.
+ */
+void _puts(char *str);
+
 #endif
