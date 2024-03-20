e09fc4a2901d7523ddce25104a76b66b429359aa
root <ntjalebohang@yahoo.com>
2024-03-20 17:04:20 +0000

    f

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ void rev_string(char *s)
 	for (a = 0; s[a] != '\0'; a++)
 		;
 	c = a;
-	for (a--, b = 0; b < c / 2; a--,b++)
+	for (a--, b = 0; b < c / 2; a--, b++)
 	{
 		h = s[b];
 		s[b] = s[a];
