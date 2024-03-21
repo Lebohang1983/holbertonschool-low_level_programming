e45bb24bba2439ad0f27c5626f461757a237ae8c
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-21 11:41:36 +0000

    H

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -48,4 +48,11 @@ void rev_string(char *s);
  */
 void puts2(char *str);
 
+/**
+ * print_array - prints an aray a number of times.
+ * @a: an array.
+ * @n: the number of elements to print.
+ */
+void print_array(int *a, int n);
+
 #endif
