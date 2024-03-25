94f6fd5cec72c527c93484cba2d22455ae1f1475
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-25 05:39:23 +0000

    K

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -140,4 +140,13 @@ char *cap_string(char *s);
  */
 char *leet(char *s);
 
+/**
+ * _memset - this funtion fills memory with a constant byte.
+ * @b: a constant byte.
+ * @n: number of bytes.
+ * @s: pointers to memory.
+ * Return: A pointer to memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n);
+
 #endif
