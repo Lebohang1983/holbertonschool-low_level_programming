a653415409952087abf624c7afdfb52108fc44a5
Lebohang1983 <ntjalebohang@yahoo.com>
2024-03-26 04:54:46 +0000

    g

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -159,4 +159,12 @@ char *_memset(char *s, char b, unsigned int n);
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _strchr - returns a point to the first occurance of a character in a string.
+ * @s: string to search for the character in.
+ * @c: char to the search for.
+ * Return: pointer to char or else NULL.
+ */
+char *_strchr(char *s, char c);
+
 #endif
