df284ba8858fbfb9932aefc00a5d2d7532dac814
Lebohang1983 <ntjalebohang@yahoo.comclear>
2024-03-26 05:08:53 +0000

    g

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -176,4 +176,12 @@ char *_strchr(char *s, char c);
  */
 unsigned int _strspn(char *s, char *accept);
 
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ * @s: haystack.
+ * @accept: needle.
+ * Return: pointer to to byte s that matches one of the byes in accept, or NUL
+ */
+char *_strpbrk(char *s, char *accept);
+
 #endif
