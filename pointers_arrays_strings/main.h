320804be254e3da698bf86364a344d990a273fcb
Lebohang1983 <ntjalebohang@yahoo.comclear>
2024-03-26 05:22:49 +0000

    g

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -200,4 +200,13 @@ char *_strstr(char *haystack, char *needle);
  */
 void print_chessboard(char (*a)[8]);
 
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of
+ * integers.
+ * @a: bad idea.
+ * @size: the size of the the array.
+ * Return: Nothing
+ */
+void print_diagsums(int *a, int size);
+
 #endif
