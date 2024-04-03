a961461011ce7942953a1872712f09613ca8aa13
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-03 16:36:58 +0000

    j

--- recursion/main.h
+++ recursion/main.h
@@ -29,4 +29,14 @@ int _strlen_recursion(char *s);
  */
 int factorial(int n);
 
+
+/**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: value to be manipulated.
+ * @y: manipulator
+ * Return: Sum as Int
+ */
+int _pow_recursion(int x, int y);
+
 #endif
