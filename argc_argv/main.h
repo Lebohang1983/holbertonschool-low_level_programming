251858bd9f68390141484ff750ae43fb78bb1ae5
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-04 10:35:09 +0000

    arg

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -7,4 +7,12 @@
  */
 int main(int argc __attribute__((unused)), char *argv[]);
 
+/**
+ * main - Entry point, Also print number of arguments starting a 0
+ * @argc: count
+ * @argv: arguments string
+ * Return: INT count of arguments
+ */
+int main(int argc, char *argv[] __attribute__((unused)));
+
 #endif
