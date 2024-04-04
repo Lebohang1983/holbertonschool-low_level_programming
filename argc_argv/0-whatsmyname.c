88b350513be4fea4a5a26c8bcefadabbb2e748ce
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-04 09:35:36 +0000

    arg

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-
 /**                                                                                      
- * main - A programme that prints its name followed by a new line                         
- * Return: Always 0 (success).                                                            
+ * main - A programme that prints its name followed by a new line                        
+ * Return: Always 0 (success)                          
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
