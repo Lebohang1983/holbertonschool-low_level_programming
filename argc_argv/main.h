f1d60e056ccd6cf3951498e13c5a7bc2c522995d
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-04 10:02:19 +0000

    arg

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 /**
- * main - A programme that prints its name followed by a new line
+ * main - Prints its name followed by a new line
  * Return: Always 0 (success).
  */
 int main(int argc __attribute__((unused)), char *argv[]);
