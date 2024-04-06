c9a81ae241978af691330890d3e2c83641f31cf1
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-06 16:56:28 +0000

    malloc

---
+++ malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * create_array - creates an array of a spacific size and fill with a char.
+ * @size : the Size of the array to make.
+ * @c: characture to fill the array with.
+ * Return: An array of charactures.
+ */
+char *create_array(unsigned int size, char c);
+
+#endif
