70943623ffca5d324b836de0f1b3f95689027ce0
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-13 13:33:16 +0000

    malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/** 
+/**
  *void *malloc_checked- function that allocates memory
  *@ptr- returned pointer
  *@i- size of string
@@ -9,7 +9,6 @@
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-
 	ptr = malloc(b);
 	if (ptr == NULL)
 	{
