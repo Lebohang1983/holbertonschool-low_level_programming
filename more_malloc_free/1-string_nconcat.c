8009834f7bc835881d7d9aeeb14be83ddd952e99
Lebohang@1983 <ntjalebohang@yahoo.com>
2024-04-13 17:07:25 +0000

    Malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,24 +8,34 @@
  * @s2: character to be concatenated
  * Return: NULL
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
+	char *str;
+	unsigned int i, a, b;
 
-	if (n >= sizeof(s2))
-	{
-		ptr = malloc(sizeof(s1) + sizeof(s2) + 1);
-	}
-	else 
-	{
-		ptr = malloc(sizeof(char) * (sizeof(s1) +  n + 1));
-	}
+	a = b = 0;
+	if (s1)
+		for (; s1[a]; a++)
+			;
+	if (s2)
+		for (; s2[b]; b++)
+			;
+	if (!(n >= b))
+		b = n;
 
-	if (ptr == NULL)
-	{
+	str = malloc(sizeof(char) * (a + b) + 1);
+	if (!str)
 		return (NULL);
+
+	for (i = 0; i < (a + b); i++)
+	{
+		if (i < a)
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - a];
 	}
 
-	return (0);
-}
+	str[i] = '\0';
 
+	return (str);
+}
