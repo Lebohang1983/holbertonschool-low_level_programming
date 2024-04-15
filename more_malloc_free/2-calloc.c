fb5cbbaf7a5c7896ae9b9470016a797c0c6347fd
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-15 12:37:30 +0000

    malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,17 +9,33 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int arr[nmemb];
-	unsigned int i;
-        int *ptr;
-	int ctr;
+   
+    size_t total_size = nmemb * size;
 
-	ctr = calloc(size(i) nmemb);
-	ptr = malloc(size of(i));
-	if {
-		(nmemb = 0 && ptr == null)
-		while ptr = 0 
-	calloc == NULL
-	}
-	_putchar(ptr)
+    void *ptr = calloc(nmemb, size);
+
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+
+    return ptr;
+}
+
+int main() {
+    // Test _calloc
+    int *arr = (int *)_calloc(5, sizeof(int));
+    if (arr != NULL) {
+       
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i * 10;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            printf("%d ", arr[i]);
+        }
+        free(arr);
+    }
+
+    return 0;
 }
