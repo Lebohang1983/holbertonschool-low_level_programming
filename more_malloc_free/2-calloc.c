96c8d7cc49202d7ec6a8471d0276b65678fbfdfe
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-16 05:49:31 +0000

    malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
    unsigned int i;
    unsigned int *ptr;
 
-   if (nmemb == 0 || i == 0)
+   if (nmemb == 0 || size == 0)
 	   return (NULL);
    ptr = malloc(nmemb * size);
    if (ptr == NULL)
