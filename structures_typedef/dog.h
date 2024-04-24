360e986330dca3cbe8e274fd5f8d9408a4ec0ec5
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-24 05:13:31 +0000

    structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - structure with elements
+ * @name: name of dog
+ * @age: dog age
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
