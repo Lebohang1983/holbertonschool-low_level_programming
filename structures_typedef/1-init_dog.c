d43fb6085bbf014765eec59548b95982a1087f8c
Lebohang1983 <ntjalebohang@gmail.com>
2024-04-25 05:19:08 +0000

    structure

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,8 +8,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	d.name = "Poppy";
-	d.age = 3.5;
-	d.owner = "Bob"
+	d->name = "Poppy";
+	d->age = 3.5;
+	d->owner = "Bob"
 }
 
