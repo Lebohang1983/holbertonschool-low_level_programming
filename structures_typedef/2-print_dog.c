936b25238099a28234f8683c43757a2ff1268bf0
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-30 05:35:02 +0000

    Struct2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,6 +16,13 @@ void print_dog(struct dog *d)
 		return;
 	}
 
+	if(Name == NULL || Age == NULL || Owner == NULL)
+	{
+		printf("Name: (nil)\n")
+		printf("Age: (nil)\n")
+		printf("Owner: (nil)\n")
+	}
+
 	printf("Name: %s\n", d->name);
 	printf("Age; %f\n", d->age);
 	printf("Owner: %s\n",d->owner);
