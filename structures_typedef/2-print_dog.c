84903eb1583251ee84eecc12ded50bd717bf8e76
Lebohang1983 <ntjalebohang@yahoo.com>
2024-04-30 05:11:10 +0000

    Struct2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,40 +1,23 @@
 #include "dog.h"
 #include <stddef.h>
 #include <stdio.h>
+
 /**
- * print_dog - function that prints struct dog
+ * print_dog - funtion that prints struct dog
  * @d: pointer to the variable struct dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
  * Return: void
  */
 
 void print_dog(struct dog *d)
- 
 {
-       	char name;
-	float age;
-	char owner;
-
-	if (d = NULL)
+	if (d == NULL)
 	{
-		printf();
+		printf("NULL ponter\n");
+		return;
 	}
 
-	if (d != NULL)
+	printf("Name: %s\n", d->name);
+	printf("Age; %f\n", d->age);
+	printf("Owner: %s\n",d->owner);
 
-	{
-		printf("%s\n",name);
-	}
-
-	if (d != NULL)
-	{
-		printf("%f\n",age);
-	}
-	if (d != NULL)
-	{
-		printf("%s\n",owner);
-	}
 }
-
